2ca9c4f016205c36abadabd3c61f52546196d640
BAN Jun <banjun@gmail.com>
2017-02-07 16:36:53 +0900

    remove Pods/ from version control

--- Example/Pods/Target Support Files/Caerula/Caerula-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Caerula : NSObject
-@end
-@implementation PodsDummy_Caerula
-@end
--- Example/Pods/Target Support Files/Pods-Caerula_Tests/Pods-Caerula_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Caerula_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Caerula_Tests
-@end
